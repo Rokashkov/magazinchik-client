120125fb41f0cd82fbce2a1357311781602901be
Vladimir <vovalol31@gmail.com>
2023-05-02 01:54:36 +0700

    добавлен редирект с /profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,25 @@
+import axios from 'axios'
+import { GetServerSideProps } from 'next'
 import { ProfilePage } from 'pages/user/ProfilePage'
 
 export default ProfilePage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	try {
+		const cookie = context.req.headers.cookie
+		const response = await axios.get(`${ process.env.NEXT_PUBLIC_API_URL }/auth/refresh`, { headers: { cookie } })
+		context.res.setHeader('set-cookie', response.headers['set-cookie'] as string[])
+
+		return {
+			props: {}
+		}
+	} catch (error) {
+		
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false
+			}
+		}
+	}
+}
--- pages/register.tsx
+++ pages/register.tsx
@@ -21,6 +21,5 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 		return {
 			props: {}
 		}
-		
 	}
 }
